9fdaee4ec85735de04ff54b0f992893a436b16b8
Anthony Urrego <anthourrego@gmail.com>
2025-11-13 23:05:48 -0500

    Cache de las imagenes

--- app/Http/Controllers/Api/EventProducts/EventProductsController.php
+++ app/Http/Controllers/Api/EventProducts/EventProductsController.php
@@ -45,11 +45,16 @@ class EventProductsController extends Controller
 
         if ($imagen && Storage::disk("public")->exists("images/products/{$imagen}")) {
             // Si existe, devuelve la imagen del producto.
-            return Storage::disk('public')->response("images/products/{$imagen}");
+            return Storage::disk('public')->response("images/products/{$imagen}")
+                ->header('Cache-Control', 'public, max-age=86400')
+                ->header('Expires', gmdate('D, d M Y H:i:s', time() + 86400) . ' GMT');
         }
 
         // 4. Si no, devuelve la imagen por defecto.
-        return response()->file($defaultImagePath);
+        return response()->file($defaultImagePath, [
+            'Cache-Control' => 'public, max-age=86400',
+            'Expires' => gmdate('D, d M Y H:i:s', time() + 86400) . ' GMT'
+        ]);
     }
 
 }
