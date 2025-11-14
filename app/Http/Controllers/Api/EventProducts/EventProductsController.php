fd376a7b2a24aad7fa2b13e730113d08e89f1215
Anthony Urrego <anthourrego@gmail.com>
2025-11-13 23:07:38 -0500

    Funca

--- app/Http/Controllers/Api/EventProducts/EventProductsController.php
+++ app/Http/Controllers/Api/EventProducts/EventProductsController.php
@@ -45,16 +45,11 @@ class EventProductsController extends Controller
 
         if ($imagen && Storage::disk("public")->exists("images/products/{$imagen}")) {
             // Si existe, devuelve la imagen del producto.
-            return Storage::disk('public')->response("images/products/{$imagen}")
-                ->header('Cache-Control', 'public, max-age=86400')
-                ->header('Expires', gmdate('D, d M Y H:i:s', time() + 86400) . ' GMT');
+            return Storage::disk('public')->response("images/products/{$imagen}");
         }
 
         // 4. Si no, devuelve la imagen por defecto.
-        return response()->file($defaultImagePath, [
-            'Cache-Control' => 'public, max-age=86400',
-            'Expires' => gmdate('D, d M Y H:i:s', time() + 86400) . ' GMT'
-        ]);
+        return response()->file($defaultImagePath);
     }
 
 }
